e11d28ec5f715850308f7b836a53b6c4c16d723a
Sujit Jukalkar <bitbyjitcoding812017@gmail.com>
2024-11-04 17:52:25 -0500

    first commit message i think?

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import fmt
+
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
+
+func main() {
+  mux := 
+  http.HandleFunc("/", greet)
+  http.ListenAndServe(":8080", nil)
+}
