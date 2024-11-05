92d482e4ccc18b4a57e937b854ed25e5b6030c6e
Sujit Jukalkar <bitbyjitcoding812017@gmail.com>
2024-11-05 01:40:23 -0500

    Web Hello completed

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
-import fmt
-
-package main
-
 import (
-  "fmt"
-  "net/http"
-  "time"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello World! %s", time.Now())
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
 func main() {
-  mux := 
-  http.HandleFunc("/", greet)
-  http.ListenAndServe(":8080", nil)
+  mux := http.NewServeMux()
+	mux.HandleFunc("/", greet)
+  http.ListenAndServe(":8080", mux)
 }
