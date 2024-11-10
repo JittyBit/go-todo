263d0570a88017ad0f6c11160719106cc6232263
Sujit Jukalkar <bitbyjitcoding812017@gmail.com>
2024-11-09 19:58:57 -0500

    added email field to User table/orm

--- models/orms.go
+++ models/orms.go
@@ -5,6 +5,7 @@ import "github.com/google/uuid"
 type User struct {
 	ID   uuid.UUID `db:"id"`
 	Name string    `db:"name" json:"name"`
+  Email string   `db:"email" json:"email"`
 }
 
 type Task struct {
